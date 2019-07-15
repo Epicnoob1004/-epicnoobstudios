8d15465904ca287b90eb2b840772f4ee17e7dde4
Epicnoob1004 <silvianoea15@gmail.com>
2019-07-15 10:38:33 -0600

    first commit

---
+++ action.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<a href="index2.html"><button>test</button></a>
+<?php
+$dogo="dog";
+$is="is";
+$my="my";
+$a="a";
+$goat="goat";
+echo "$my $dogo $is $a $goat";
+
+?>
+</body>
+</html>
